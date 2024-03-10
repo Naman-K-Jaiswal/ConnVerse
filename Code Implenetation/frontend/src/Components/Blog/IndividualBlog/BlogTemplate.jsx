d356019463d86358b53a21c93d958ee50121cab1
spriyanshu22 <spriyanshu22@iitk.ac.in>
2024-03-10 16:15:52 +0530

    added dropBox, added Upvote downvote n comments, adjust commentList height, and some changes on home page

--- Code Implenetation/frontend/src/Components/Blog/IndividualBlog/BlogTemplate.jsx
+++ Code Implenetation/frontend/src/Components/Blog/IndividualBlog/BlogTemplate.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './style.css';
 
+
 const BlogTemplate = () => {
+
+  const [upvoted, setUpvoted] = useState(false);
+  const [downvoted, setDownvoted] = useState(false);
+
+  const handleUpvote = () => {
+    if (!upvoted) {
+      setUpvoted(true);
+      setDownvoted(false);
+    } else {
+      setUpvoted(false);
+    }
+  };
+
+  const handleDownvote = () => {
+    if (!downvoted) {
+      setDownvoted(true);
+      setUpvoted(false);
+    } else {
+      setDownvoted(false);
+    }
+  };
+
   const blogData = [
     {
       id: 1,
       title: "Mastering the art of Leadership",
       username: "User12",
       lastDate: "13 days ago",
+      upvotes: "10",
+      noOfComments: "12",
       profileImageSrc: require('./blog_img.jpeg'),
-      blogImages: [require('./blog_img.jpeg'),require('./blog_img.jpeg'),require('./blog_img.jpeg'),require('./blog_img.jpeg'), require('./blog_img.jpeg'), require('./blog_img.jpeg'), require('./blog_img.jpeg'), require('./blog_img.jpeg')],
+      blogImages: require('./blog_img.jpeg'),
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque r Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque re Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque re Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque re Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque reLorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum harum minus nobis at laborum qui et tempora. Sequi dolorum iste amet vitae harum exercitationem deserunt vel doloremque eveniet facere.  Maiores ipsam quasi officiis atque error magnam facilis enim, perspiciatis voluptas illum delectus aperiam ipsa quos sed harum recusandae totam porro ab? At nisi est facilis necessitatibus nemo, officiis cumque!Esse veritatis illum delenitcusantium deserunt tempore quis impedit sit aliquid minima non, mollitia neque harum modi esse maiores libero cum. Minus similique officia ratione est porro! Incidunt, cumque recumque recumque reepudiandae"
 ,
       comments: [
@@ -51,27 +76,49 @@ const BlogTemplate = () => {
               <div id="titleBlogDiv">
                 {blog.title}
               </div>
-              <div id="usernameBlogDiv">
-                {blog.username}
-              </div>
-              <div id="uploadedDayBlogDiv">
-                {blog.lastDate}
+              <div id="subTitleBlogDiv">
+                    {/* subTitleBlogDiv for displaying : userDetails(username and upload time) & #upvotes #comments */}
+                    <div id="userDetailsBlogDiv">
+                      <div id="usernameBlogDiv">
+                        {blog.username}
+                      </div>
+                      <div id="uploadedDayBlogDiv">
+                        {blog.lastDate}
+                      </div>
+                    </div>
+                    
+                    <div id="votesNCommentsBlogDiv">
+                      <div id="upVotesBlogDiv">
+                        <button id="upVoteButton" onClick={handleUpvote} style={{ color: upvoted ? '#e2921b' : 'black' }}>
+                          &#x21e7;
+                        </button>
+                        {blog.upvotes} 
+                      </div>
+                      <div id="downVotes">
+                        <button id="downVoteButton" onClick={handleDownvote} style={{ color: downvoted ? 'red' : 'black' }}>
+                        &#x21e9;
+                        </button>
+                      </div>
+                      <div id="comments">
+                        Comments: {blog.noOfComments}
+                      </div>
+                    </div>
+
               </div>
             </div>
           </div>
           <div id="imagesBlogDiv">
-            {blog.blogImages.map((image, index) => (
-              <img key={index} src={image} alt={`Blog Image ${index}`} />
-            ))}
+          <img src={blog.blogImages} alt="" />
           </div>
           <div id="contentBlogDiv">
             {blog.content}
           </div>
         </div>
       ))}
+      
       <div id="rightHalfCommentsDiv">
         <div id="headingCommentsDiv">
-          COMMENTS:
+          Comments:
         </div>
         <div id="commentsListDiv">
           {blogData.map((blog) => (
--- Code Implenetation/frontend/src/Components/Navbar/Navbar.jsx
+++ Code Implenetation/frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import GroupIcon from '@mui/icons-material/People';
 import CreateIcon from '@mui/icons-material/Create';
@@ -9,11 +9,19 @@ import profileImg from './profile_pic.JPG';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLogout = () => {
+    // Perform logout action
+    console.log('Logging out...');
+  };
+
   return (
     <nav className="navbar">
       <div className='connversehead'>
       <p id='Conn1'>ConnVerse</p>
       </div>
+      
       <ul className="nav-items">
         <li className="nav-item">
           <Link to="/home" className="nav-link">
@@ -42,8 +50,19 @@ const Navbar = () => {
       </ul>
       <div className="profile">
         <img src={profileImg} alt="Profile Photo" className="profile-photo"/>
-        <span className="username">priyanshu799</span>
+        <div className="dropdown">
+          <span className="username">priyanshu799</span>
+          <button className="dropbtn" onClick={() => setIsOpen(!isOpen)}>&#x25BC;</button>
+          {isOpen && (
+            <div className="dropdown-content">
+              <Link to="/profile">My Profile</Link>
+              <Link to="/">Log Out</Link>
+            </div>
+          )}
+        </div>
       </div>
+
+
     </nav>
   );
 }
